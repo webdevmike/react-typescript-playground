1391779a7ea752c9c7e7e492d27af279d967660f
webdevmike <mikebostone@gmail.com>
2026-06-04 20:38:30 -0500

    added all form fields and reset button

--- app/routes/FormWithValidation/Form.tsx
+++ app/routes/FormWithValidation/Form.tsx
@@ -14,19 +14,23 @@ type FormFields = {
 
 type FormErrors = Record<keyof FormFields, string>;
 
+const INITIAL_FIELDS: FormFields = {
+  name: "",
+  shirtSize: "",
+  attendanceType: "",
+  codeOfConduct: false,
+};
+
+const INITIAL_ERRORS: FormErrors = {
+  name: "",
+  shirtSize: "",
+  attendanceType: "",
+  codeOfConduct: "",
+};
+
 export default function Form() {
-  const [fields, setFields] = useState<FormFields>({
-    name: "",
-    shirtSize: "",
-    attendanceType: "",
-    codeOfConduct: false,
-  });
-  const [errors, setErrors] = useState<FormErrors>({
-    name: "",
-    shirtSize: "",
-    attendanceType: "",
-    codeOfConduct: "",
-  });
+  const [fields, setFields] = useState<FormFields>(INITIAL_FIELDS);
+  const [errors, setErrors] = useState<FormErrors>(INITIAL_ERRORS);
 
   const handleSubmit = (e: React.SubmitEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -62,6 +66,19 @@ export default function Form() {
       newErrors.name = "Name is required";
     }
 
+    if (fields.shirtSize === "") {
+      newErrors.shirtSize = "Shirt size is required";
+    }
+
+    if (fields.attendanceType === "") {
+      newErrors.attendanceType = "Attendance type is required";
+    }
+
+    if (fields.codeOfConduct === false) {
+      newErrors.codeOfConduct =
+        "You must agree to abide by our code of conduct";
+    }
+
     setErrors(newErrors);
 
     return Object.values(newErrors).every((field) => field.length === 0);
@@ -71,16 +88,97 @@ export default function Form() {
     <div className={styles.wrapper}>
       <h1>Form with Validation</h1>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={fields.name}
-          onChange={handleFieldChange}
-        />
-        {errors.name && <div className={styles.error}>{errors.name}</div>}
-        <button type="submit">Send</button>
+        <div className={styles.row}>
+          <label htmlFor="name">Name:</label>
+          <div className={styles.fieldInput}>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={fields.name}
+              onChange={handleFieldChange}
+            />
+            {errors.name && <div className={styles.error}>{errors.name}</div>}
+          </div>
+        </div>
+        <div className={styles.row}>
+          <label htmlFor="shirt-size">Shirt Size:</label>
+          <div className={styles.fieldInput}>
+            <select
+              name="shirtSize"
+              id="shirt-size"
+              value={fields.shirtSize}
+              onChange={handleFieldChange}
+            >
+              <option value="">Select a size</option>
+              <option value="S">Small</option>
+              <option value="M">Medium</option>
+              <option value="L">Large</option>
+            </select>
+            {errors.shirtSize && (
+              <div className={styles.error}>{errors.shirtSize}</div>
+            )}
+          </div>
+        </div>
+        <fieldset>
+          <legend>Attendance Type:</legend>
+          <div className={styles.row}>
+            <div className={styles.radioInput}>
+              <input
+                type="radio"
+                value="In Person"
+                id="in-person"
+                name="attendanceType"
+                checked={fields.attendanceType === "In Person"}
+                onChange={handleFieldChange}
+              />
+              <label htmlFor="in-person">In Person</label>
+            </div>
+            <div className={styles.radioInput}>
+              <input
+                type="radio"
+                value="Virtual"
+                id="virtual"
+                name="attendanceType"
+                checked={fields.attendanceType === "Virtual"}
+                onChange={handleFieldChange}
+              />
+              <label htmlFor="virtual">Virtual</label>
+            </div>
+          </div>
+          {errors.attendanceType && (
+            <div className={styles.error}>{errors.attendanceType}</div>
+          )}
+        </fieldset>
+        <div className={styles.row}>
+          <input
+            type="checkbox"
+            id="code-of-conduct"
+            name="codeOfConduct"
+            checked={fields.codeOfConduct}
+            onChange={handleFieldChange}
+          />
+          <div className={styles.fieldInput}>
+            <label htmlFor="code-of-conduct">
+              I agree to the code of conduct
+            </label>
+            {errors.codeOfConduct && (
+              <div className={styles.error}>{errors.codeOfConduct}</div>
+            )}
+          </div>
+        </div>
+        <div className={styles.buttons}>
+          <button type="submit">Send</button>
+          <button
+            type="button"
+            onClick={() => {
+              setFields(INITIAL_FIELDS);
+              setErrors(INITIAL_ERRORS);
+            }}
+          >
+            Reset
+          </button>
+        </div>
       </form>
     </div>
   );
